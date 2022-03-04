79aac1ae6654113a79dbcceb7ff990611e88cb09
Shiwen Cheng <912284855@qq.com>
2022-03-04 21:58:47 +0800

    add genre list group on left side.

---
+++ src/components/common/list_group.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { genres } from "../../services/fakeGenreService";
+
+const ListGroup = (props) => {
+  const { onSelect, currentGenre } = props;
+  const genres = [{ _id: "0", name: "All Genres" }, , ...props.genres];
+
+  console.log(currentGenre);
+  return (
+    <ul className="list-group">
+      {genres.map((g) => (
+        <li key={g._id} onClick={onSelect} className="list-group-item">
+          {g.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListGroup;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react"; // imrc
 import { getMovies } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
+import ListGroup from "../components/common/list_group";
 
 class Movies extends React.Component {
   // cc
   state = {
     movies: getMovies(),
+    genres: getGenres(),
+    currentGenre: "All Genres",
     currentPage: 1,
     pageSize: 4,
   };
@@ -29,9 +33,19 @@ class Movies extends React.Component {
     this.setState({ currentPage: page });
   };
 
+  handleSelect = () => {
+    console.log("select");
+  };
+
   render() {
     const { length: count } = this.state.movies;
-    const { movies: allMovies, pageSize, currentPage } = this.state;
+    const {
+      movies: allMovies,
+      genres,
+      currentGenre,
+      pageSize,
+      currentPage,
+    } = this.state;
 
     if (count === 0) return <p>there is no movie.</p>;
 
@@ -39,48 +53,62 @@ class Movies extends React.Component {
 
     return (
       <React.Fragment>
-        <p>showing {count} movies in the database.</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((m) => (
-              <tr key={m._id}>
-                <td>{m.title}</td>
-                <td>{m.genre.name}</td>
-                <td>{m.numberInStock}</td>
-                <td>{m.dailyRentalRate}</td>
-                <td>
-                  <Like onClick={() => this.handleLike(m)} liked={m.liked} />
-                  {/* 写在使用组件这个位置的属性，是传参
+        <div className="row">
+          <div className="col-3">
+            <ListGroup
+              genres={genres}
+              currentGenre={currentGenre}
+              onSelect={this.handleSelect}
+            />
+          </div>
+          <div className="col">
+            <p>showing {count} movies in the database.</p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th />
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((m) => (
+                  <tr key={m._id}>
+                    <td>{m.title}</td>
+                    <td>{m.genre.name}</td>
+                    <td>{m.numberInStock}</td>
+                    <td>{m.dailyRentalRate}</td>
+                    <td>
+                      <Like
+                        onClick={() => this.handleLike(m)}
+                        liked={m.liked}
+                      />
+                      {/* 写在使用组件这个位置的属性，是传参
                   写在构建组件位置的作用，才是发起事件 */}
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(m)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
+                    </td>
+                    <td>
+                      <button
+                        onClick={() => this.handleDelete(m)}
+                        className="btn btn-danger btn-sm"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            />
+          </div>
+        </div>
       </React.Fragment>
     );
   }
