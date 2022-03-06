ab1123fdeeb6114559b5ce4badffd7297ecbc06f
Shiwen Cheng <912284855@qq.com>
2022-03-06 14:05:36 +0800

    add loginForm.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <main className="container">
         <Switch>
           {/* Route[path][component]*4 */}
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,23 +19,20 @@ const NavBar = (props) => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/movies">
-              Movies <span className="sr-only">(current)</span>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/customers">
-              Customers
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/rentals">
-              Rentals
-            </NavLink>
-          </li>
-        </ul>
+        <div className="navbar-nav">
+          <NavLink className="nav-link nav-item" to="/movies">
+            Movies <span className="sr-only">(current)</span>
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/rentals">
+            Rentals
+          </NavLink>
+          <NavLink className="nav-link nav-item" to="/login">
+            Login
+          </NavLink>
+        </div>
       </div>
     </nav>
     // <div classNameName="nav nav-pills card-header-pills m-2">
