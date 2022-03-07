6a5e6d35ea327ab093bff4be102d7a6e0e25ba6c
Shiwen Cheng <912284855@qq.com>
2022-03-07 12:35:03 +0800

    extract a reusable form.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./common/input";
-class LoginForm extends Component {
+import Form from "./common/form";
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     // 创建表单一定要初始化 state
     errors: {},
   };
@@ -19,53 +20,15 @@ class LoginForm extends Component {
   //     this.username.current.focus();
   //   }
 
-  validate = () => {
-    const options = {
-      abortEarly: false,
-    };
-
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message; // error.details.map((item) => (errors[item.path[0]] = item.message));
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault(); // 阻止表单提交的默认行为
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} }); // 如果 errors 是 truthy，就用errors，如果不是，就用空{}
-    if (errors) return;
-
+  doSubmit = () => {
     //   call the server
     // const username = this.username.current.value;
     console.log("Submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
+    const { data, errors } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -73,14 +36,14 @@ class LoginForm extends Component {
           <Input
             name="username"
             label="Username"
-            value={account.username}
+            value={data.username}
             onChange={this.handleChange}
             error={errors.username}
           ></Input>
           <Input
             name="password"
             label="Password"
-            value={account.password}
+            value={data.password}
             onChange={this.handleChange}
             error={errors.password}
           ></Input>
