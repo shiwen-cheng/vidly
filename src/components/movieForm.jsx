58d35a89f0f6dc503f77ceda7da351f4001c503d
Shiwen Cheng <912284855@qq.com>
2022-03-08 20:45:14 +0800

    refactor.

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -27,20 +27,27 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id; // 读取路由中的 id 参数
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id; // 读取路由中的 id 参数
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
